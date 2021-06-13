38576db8bdea2b3c5163dcb9d7fa8d473836ee21
lichenguang <lichenguang@smzdm.com>
2021-06-13 18:42:59 +0800

    commit teacher v1 code files

--- snippets_dataset.py
+++ snippets_dataset.py
@@ -21,10 +21,12 @@ class SnippetsDataset(Dataset):
     def __getitem__(self, index):
         file, label = self.files[index], self.labels[index]
         data = np.load(file).astype(float)  # before - dtype('float16'); after - dtype('float64')
-        data = data / np.array([np.abs(data).max() for i in range(6)]).reshape(6, 1, 1)
+        data = np.vstack(data).transpose(1, 0)
+        data = np.expand_dims(data, axis=0)
 
         if self.transform:
             data = self.transform(data)
+
         return data, label
 
     def __len__(self):
@@ -33,30 +35,34 @@ class SnippetsDataset(Dataset):
     def load(self, labels_csv_file):
         df = pd.read_csv(labels_csv_file)
         df['file'] = df['id'].apply(lambda x: os.path.join(self.data_path, x[0], f'{x}.npy'))
-        if self.mode == 'train':
-            df = df.sample(frac=self.train_valid_rate, random_state=self.seed)
-        elif self.mode == 'valid':
-            df = df.sample(frac=1 - self.train_valid_rate, random_state=self.seed)
+
+        if self.mode == 0:
+            df = df[df.target == 0]
+        elif self.mode == 1:
+            df = df[df.target == 1]
         else:
             raise Exception('', '')
+
         return df.file.tolist(), df.target.tolist()
 
 
 class SnippetsDatasetTest(Dataset):
 
-    def __init__(self, data_path, transform=None, seed=123):
+    def __init__(self, data_path, mode, transform=None, seed=123):
         random.seed(seed)
         self.data_path = data_path
+        self.mode = mode
         self.transform = transform
         self.files = self.load()
 
     def __getitem__(self, index):
         file = self.files[index]
-        data = np.load(file).astype(float)
-        data = data / np.array([np.abs(data).max() for i in range(6)]).reshape(6, 1, 1)
+        data = np.load(file).astype(float)  # before - dtype('float16'); after - dtype('float64')
+        data = np.vstack(data).transpose(1, 0)
+        data = np.expand_dims(data, axis=0)
         if self.transform:
             data = self.transform(data)
-        return data
+        return data, file
 
     def __len__(self):
         return len(self.files)
@@ -68,7 +74,12 @@ class SnippetsDatasetTest(Dataset):
                 continue
             folder_path = os.path.join(self.data_path, folder)
             folder_files = os.listdir(folder_path)
-            random_file = random.choice(folder_files)
-            files.append(os.path.join(folder_path, folder_files[0]))
-            files.append(os.path.join(folder_path, random_file))
+            if self.mode == 'test':
+                random_file = random.choice(folder_files)
+                files.append(os.path.join(folder_path, folder_files[0]))
+                files.append(os.path.join(folder_path, random_file))
+            elif self.mode == 'inference':
+                files.extend([os.path.join(folder_path, file) for file in folder_files])
+            else:
+                raise Exception('', '')
         return files
--- train_copy_assitant_1.py
+++ train_copy_assitant_1.py
@@ -2,7 +2,7 @@
 1、借鉴老师代码V1
 2、解读他的代码思路
  - trian_labels.csv 的读取
- - 
+ -
 3、我未曾用过的库
  - glob - 可使用相对路径的库？| 可按照Unix终端所使用的那般规则来查询文件等
 '''
@@ -25,13 +25,16 @@ r_ = Fore.WHITE
 
 import torch
 from torch import nn, optim
-from torch.utils.data import DataLoader
+from torch.utils.data import DataLoader, ConcatDataset, Subset
 from torch.utils.tensorboard import SummaryWriter
 import torchvision
 from torchvision import transforms
+from ignite.contrib.metrics import ROC_AUC
 from efficientnet_pytorch import EfficientNet, utils
+
 from sklearn import metrics
 from sklearn.model_selection import StratifiedKFold
+
 
 import plotly.express as px
 import plotly.graph_objects as go
@@ -45,7 +48,7 @@ from plotly.subplots import make_subplots
 # from skimage.io import imshow, imread, imsave
 # from skimage.transform import rotate, AffineTransform, warp, rescale, resize, downscale_local_mean
 
-from snippets_dataset import SnippetsDataset, SnippetsDatasetTest
+from snippets_dataset import SnippetsDataset, SnippetsDatasetTest, SimpleCustomBatch
 
 
 logger.add('/home/alben/code/kaggle_SETI_search_ET/log/train.log', rotation="1 day")
@@ -74,7 +77,7 @@ class EfficientNetV2(nn.Module):
         self.efficientnet = EfficientNet.from_pretrained(backbone)
         fc_in_feature = self.efficientnet._fc.in_features
         self.efficientnet._fc = nn.Linear(fc_in_feature, out_dim)
-        self.conv1_6_3 = nn.Conv2d(6, 3, kernel_size=3, stride=1, padding=3, bias=False)
+        self.conv1_6_3 = nn.Conv2d(1, 3, kernel_size=3, stride=1, padding=3, bias=False)
 
     def forward(self, x):
         x = self.conv1_6_3(x)
@@ -91,10 +94,7 @@ def mixup_data(x, y, alpha=1.0, use_cuda=True):
     else:
         lam = 1
     batch_size = x.size()[0]
-
-    index = torch.randperm(batch_size)
-    if use_cuda:
-        index = index.cuda()
+    index = torch.randperm(batch_size).cuda()
 
     mixed_x = lam * x + (1 - lam) * x[index, :]
     y_a, y_b = y, y[index]
@@ -102,13 +102,12 @@ def mixup_data(x, y, alpha=1.0, use_cuda=True):
 
 
 def mixup_criterion(criterion, pred, y_a, y_b, lam):
-    return lam * criterion(pred, y_a.view(-1, 1)) + (1 - lam) * criterion(pred, y_b.view(-1, 1))
+    return lam * criterion(pred, y_a) + (1 - lam) * criterion(pred, y_b)
 
 
 def train(epoch, model, train_loader, optimizer, loss_fn, lr_scheduler):
     losses_train, accuracy_train = [], []
     Y, y_pred = [], []
-
     model.train()
     for i, data in enumerate(train_loader):
         s_t = time.time()
@@ -116,13 +115,18 @@ def train(epoch, model, train_loader, optimizer, loss_fn, lr_scheduler):
         images, labels = images.type(torch.FloatTensor), labels.type(torch.FloatTensor)
         images, labels = images.to(device), labels.to(device)
 
-        batch_n, crops_n, C, H, W = images.shape
-        images = images.view(-1, C, H, W)
-        labels = labels.repeat_interleave(crops_n)
+        # batch_n, C, H, W = images.shape
+        # images = images.view(-1, C, H, W)
+        # labels = labels.repeat_interleave(crops_n)
 
-        outputs = model(images).squeeze(1)
         optimizer.zero_grad()
-        loss = loss_fn(outputs, labels)
+        if np.random.randint(1, 10) >= 5:
+            minxed_images, labels, choice_label, lambda_ = mixup_data(images, labels, alpha=0.4)
+            outputs = model(minxed_images).squeeze(1)
+            loss = mixup_criterion(loss_fn, outputs, labels, choice_label, lambda_)
+        else:
+            outputs = model(images)
+            loss = loss_fn(outputs, labels)
         loss.backward()
         optimizer.step()
 
@@ -139,7 +143,6 @@ def train(epoch, model, train_loader, optimizer, loss_fn, lr_scheduler):
 def valid(epoch, model, valide_loader):
     losses_valid, accuracy_valid = [], []
     Y, y_pred = [], []
-
     model.eval()
     with torch.no_grad():
         for i, data in enumerate(valide_loader):
@@ -148,16 +151,16 @@ def valid(epoch, model, valide_loader):
             images, labels = images.type(torch.FloatTensor), labels.type(torch.FloatTensor)
             images, labels = images.to(device), labels.to(device)
 
-            batch_n, crops_n, C, H, W = images.shape
-            images = images.view(-1, C, H, W)
+            # batch_n, crops_n, C, H, W = images.shape
+            # images = images.view(-1, C, H, W)
 
             outputs = model(images).squeeze(1)
-            outputs_avg = outputs.view(batch_n, crops_n).mean(1)
-            loss = loss_fn(outputs_avg, labels)
+            # outputs_avg = outputs.view(batch_n, crops_n).mean(1)
+            loss = loss_fn(outputs, labels)
 
             losses_valid.append(loss.item())
             Y.extend(labels.detach().cpu().numpy().tolist())
-            y_pred.extend(outputs_avg.detach().cpu().numpy().tolist())
+            y_pred.extend(outputs.detach().cpu().numpy().tolist())
         logger.info(f'Valid - Epoch = {epoch:3}; Iteration = {i:3}; Len = {len(valide_loader):3}; Loss = {np.mean(losses_valid):8.4}; Acc = {metrics.roc_auc_score(Y, y_pred):8.4}; Interval = {time.time() - s_t:8.4}')
     return losses_valid, metrics.roc_auc_score(Y, y_pred)
 
@@ -167,15 +170,18 @@ def test(epoch, model, test_loader, writer):
     model.eval()
     with torch.no_grad():
         for i, data in enumerate(test_loader):
-            images = data
+            images, files = data
             images = images.type(torch.FloatTensor)
             images = images.to(device)
 
-            batch_n, crops_n, C, H, W = images.shape
-            grid_images = images[:, 0, :, :, :]
-            images = images.view(-1, C, H, W)
-            outputs = model(images)
-            outputs_avg = outputs.view(batch_n, crops_n).mean(1)
+            # batch_n, crops_n, C, H, W = images.shape
+            # grid_images = images[:, 0, :, :, :]
+            # images = images.view(-1, C, H, W)
+            # outputs = model(images)
+            # outputs_avg = outputs.view(batch_n, crops_n).mean(1)
+
+            grid_images = images
+            outputs_avg = model(images)
 
             if flag == 0:
                 grid_images = grid_images.contiguous().view(-1, 1, grid_images.shape[-2], grid_images.shape[-1])
@@ -185,10 +191,20 @@ def test(epoch, model, test_loader, writer):
             logger.info(f'Test - Epoch = {epoch:3}; predict = {outputs_avg}')
 
 
-def transforms_(data):
+def transforms_1(data):
     data = torch.from_numpy(data)
     ten_datas = transforms.TenCrop(224, vertical_flip=False)(data)
     return torch.stack(ten_datas)
+
+
+def transforms_0(data):
+    data = torch.from_numpy(data)
+    data = transforms.Resize(256)(data)
+    return data
+
+
+def collate_wrapper(batch):
+    return SimpleCustomBatch(batch)
 
 
 if __name__ == '__main__':
@@ -206,47 +222,64 @@ if __name__ == '__main__':
     comment = args.comment
     logger.info(f'{"*" * 25} version = {ver}; comment = {comment} {"*" * 25}')
 
-    EPOCH = 4
-    BATCH_SIZE = 10
+    EPOCH = 30
+    BATCH_SIZE = 50
     PRINT_INTERVAL = 1000
     TRAIN_DATA_PATH = '/home/alben/data/cv_listen_2021_06/train'
     TEST_DATA_PATH = '/home/alben/data/cv_listen_2021_06/test'
     LABELS_CSV = '/home/alben/data/cv_listen_2021_06/train_labels.csv'
     BOARD_PATH = '/home/alben/code/kaggle_SETI_search_ET/board/train'
     MODEL_SAVE_PATH = '/home/alben/code/kaggle_SETI_search_ET/model_state'
-    LR = 0.01
-    LR_DECAY_STEP = 6
+    # LR = 0.01
+    # LR_DECAY_STEP = 5
     NUM_CLASSES = 1
-    baseline_name = 'efficientnet-b3'
-    normalize_mean, normalize_std = [0.485, 0.456, 0.406], [0.229, 0.224, 0.225]
-    IMG_H_W = (273, 256)
+    baseline_name = 'efficientnet-b2'
+    # normalize_mean, normalize_std = [0.485, 0.456, 0.406], [0.229, 0.224, 0.225]
+    # IMG_H_W = (273, 256)
+    TRAIN_VALID_RATE = 0.8
 
     writer = SummaryWriter(log_dir=BOARD_PATH, flush_secs=60)
 
-    train_data = SnippetsDataset(TRAIN_DATA_PATH, LABELS_CSV, 'train', transforms_)
+    label_0_data = SnippetsDataset(TRAIN_DATA_PATH, LABELS_CSV, 0)
+    label_0_ids = range(len(label_0_data))
+    label_0_train = random.sample(label_0_ids, int(len(label_0_data) * TRAIN_VALID_RATE))
+    label_0_valid = list(set(label_0_ids) - set(label_0_train))
+
+    label_1_data = SnippetsDataset(TRAIN_DATA_PATH, LABELS_CSV, 1)
+    label_1_ids = range(len(label_1_data))
+    label_1_train = random.sample(label_1_ids, int(len(label_1_data) * TRAIN_VALID_RATE))
+    label_1_valid = list(set(label_1_ids) - set(label_1_train))
+
+    train_data = ConcatDataset([Subset(label_0_data, label_0_train), Subset(label_1_data, label_1_train)])
     train_loader = DataLoader(dataset=train_data,
                               batch_size=BATCH_SIZE,
                               shuffle=True)
 
-    valid_data = SnippetsDataset(TRAIN_DATA_PATH, LABELS_CSV, 'valid', transforms_)
+    valid_data = ConcatDataset([Subset(label_0_data, label_0_valid), Subset(label_1_data, label_1_valid)])
     valid_loader = DataLoader(dataset=valid_data,
                               batch_size=BATCH_SIZE,
                               shuffle=True)
 
-    test_data = SnippetsDatasetTest(TEST_DATA_PATH, transforms_)
+    test_data = SnippetsDatasetTest(TEST_DATA_PATH, 'test')
     test_loader = DataLoader(dataset=test_data,
                              batch_size=len(test_data))
 
-    # model = EfficientNet.from_pretrained(baseline_name)
-    # model._conv_stem = first_layer
-    # fc_in_feature = model._fc.in_features
-    # model._fc = nn.Linear(fc_in_feature, NUM_CLASSES, bias=True)
-    model = EfficientNetV2(baseline_name, NUM_CLASSES)
+    model = EfficientNet.from_pretrained(baseline_name)
+    fc_in_feature = model._fc.in_features
+    model._fc = nn.Linear(fc_in_feature, NUM_CLASSES, bias=True)
+    # model = EfficientNetV2(baseline_name, NUM_CLASSES)
     model.to(device)
 
     loss_fn = nn.BCEWithLogitsLoss()
-    optimizer = optim.SGD(model.parameters(), lr=LR, momentum=0.9)
-    lr_scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=LR_DECAY_STEP, gamma=0.5)
+    optimizer = torch.optim.Adam(model.parameters())
+    scheduler = torch.optim.lr_scheduler.CyclicLR(optimizer,
+                                                  base_lr=0.001,
+                                                  max_lr=5e-4,
+                                                  gamma=0.9,
+                                                  cycle_momentum=False,
+                                                  step_size_up=1400,
+                                                  step_size_down=1400,
+                                                  mode="triangular2")
 
     for epoch in tqdm(range(EPOCH)):
         losses_train, acc_train = train(epoch, model, train_loader, optimizer, loss_fn, lr_scheduler)
@@ -255,7 +288,8 @@ if __name__ == '__main__':
                                            'valid': np.mean(losses_valid)}, epoch)
         writer.add_scalars(f'accuracy_{ver}', {'train': acc_train,
                                                'valid': acc_valid}, epoch)
+        writer.add_scalar(f'lr_{ver}',  lr_scheduler.get_last_lr()[0], epoch)
         test(epoch, model, test_loader, writer)
-        torch.save(model.state_dict(), f'{MODEL_SAVE_PATH}/vggnet_cat_dog_0525_{epoch}.pth')
-        logger.info(f'Summary - Epoch = {epoch:3}; loss_train = {np.mean(losses_train):8.4}; loss_valid = {np.mean(losses_valid):8.4}; acc_train = {acc_train:8.4}; acc_valid = {acc_valid:8.4}')
+        torch.save(model.state_dict(), f'{MODEL_SAVE_PATH}/efficientnet_SETI_0609_{epoch}.pth')
+        logger.info(f'Summary - Epoch = {epoch:3}; loss_train = {np.mean(losses_train):8.4}; loss_valid = {np.mean(losses_valid):8.4}; acc_train = {np.mean(acc_train):8.4}; acc_valid = {np.mean(acc_valid):8.4}')
     writer.close()
